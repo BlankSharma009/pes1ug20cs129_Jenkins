fd35ce49abb6e0a249c03a0b6f2c3402d349339c
BlankSharma009 <91598906+BlankSharma009@users.noreply.github.com>
2023-02-17 09:21:49 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"This code is run by PES1UG20CS129\n";
+  cout<<"This code is run by PES1UG20CS129 which is me \n";
   cout << "I have successfully built and run\n";
   return 0;
 }
